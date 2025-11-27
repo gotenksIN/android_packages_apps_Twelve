7d5cf6117303e9edcb45ecb61398994c4f6f907c
Omkar Chandorkar <gotenksIN@aospa.co>
2025-11-27 15:53:48 +0900

    Revert "Twelve: Update gradle-generatebp to 1.25"
    This reverts commit 10535cc0a960fbee926b1b0169d9f145f792cf35.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,7 +89,11 @@ dependencies {
     implementation(libs.androidx.viewpager2)
     implementation(libs.coil)
     implementation(libs.coil.network.okhttp)
-    implementation(libs.kotlinx.coroutines.guava)
+    implementation(libs.kotlinx.coroutines.guava) {
+        // Exclude compile time dependencies
+        exclude("com.google.j2objc", "j2objc-annotations")
+        exclude("org.checkerframework", "checker-qual")
+    }
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.material)
     implementation(libs.nier.visualizer) {
@@ -109,7 +113,12 @@ configure<GenerateBpPluginExtension> {
             }
             module.group.startsWith("org.jetbrains") -> true
             module.group == "com.google.android.material" -> true
+            module.group == "com.google.auto.value" -> true
+            module.group == "com.google.code.findbugs" -> true
+            module.group == "com.google.errorprone" -> true
             module.group == "com.google.guava" -> true
+            module.group == "org.jspecify" -> true
+            module.group == "junit" -> true
             else -> false
         }
     }
