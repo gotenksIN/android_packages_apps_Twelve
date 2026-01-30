aed57535bbc8664ba86ee3c2d681226de3f3f14c
Omkar Chandorkar <gotenksIN@aospa.co>
2026-01-30 12:48:06 +0530

    Revert "Twelve: Update SDK to 36"
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     defaultConfig {
         applicationId = "org.lineageos.twelve"
         minSdk = 30
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
